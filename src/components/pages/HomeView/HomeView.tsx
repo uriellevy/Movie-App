2649be76a71333363579eb835392f3abcb4afb00
uriellevy <uriellevy4892@gmail.com>
2022-07-28 11:12:29 +0300

    infinite scroll added

--- src/components/pages/HomeView/HomeView.tsx
+++ src/components/pages/HomeView/HomeView.tsx
@@ -6,9 +6,11 @@ import HomeViewListItem from './HomeViewListItem';
 import { MovieContext } from '../../store/movieContext';
 import "./HomeView.scss";
 import { Movie } from '../../types/types';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 
 const HomeView = () => {
-  const {top250Movies, searchByName, searchByNameChangeHandler} = useContext(MovieContext)
+  const { top250Movies, searchByName, searchByNameChangeHandler } = useContext(MovieContext)
 
   return (
     <div className="homeview-container">
@@ -21,16 +23,23 @@ const HomeView = () => {
         />
       </div>
       <h1>Top 250 movies</h1>
+      <InfiniteScroll
+        dataLength={top250Movies.length}
+        next={top250Movies}
+        hasMore={true}
+        loader={<Rings color='#00BFFF' height={80} width={80} />}
+      >
 
-      <ul className='movie-list-wrapper'>
-        {top250Movies.length !== 0 ?
-        top250Movies.filter((movie: Movie) => movie.title.toLocaleLowerCase().includes(searchByName)).map((movie: Movie) => (
-          <HomeViewListItem key={movie.id} movie={movie}/>
-        )):
-        <Rings color="#00BFFF" height={80} width={80} />
-        }
-        
-      </ul>
+        <ul className='movie-list-wrapper'>
+          {top250Movies.length !== 0 ?
+            top250Movies.filter((movie: Movie) => movie.title.toLocaleLowerCase().includes(searchByName)).map((movie: Movie) => (
+              <HomeViewListItem key={movie.id} movie={movie} />
+            )) :
+            <Rings color="#00BFFF" height={80} width={80} />
+          }
+
+        </ul>
+      </InfiniteScroll>
 
     </div>
   )
